747e2dad7d84080d924a78cac949933a5097c437
Salvador <Salvador@Salvador-PC>
2015-10-29 09:19:09 +0000

    Copia de fichero 1

--- src/practica/pkg4/is2/Main.java
+++ src/practica/pkg4/is2/Main.java
@@ -1,17 +1,39 @@
 package practica.pkg4.is2;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         String pathName = "C:\\";
         File file = new File (pathName);
         String[] dire = file.list();
-        for(String dire1: dire){
-            System.out.println(dire1);
-        }
+                
+        String from = "C:\\Users\\Salvador\\Pictures\\programando.jpg";
+        String to = "C:\\Users\\Salvador\\Pictures\\programando(1).jpg";
         
-        print (file.listFiles(), "");
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        
+        Date date = new Date();
+        while(true){
+            int read = input.read();
+            if(read < 0) break;
+            output.write(read);
+        }
+        System.out.println(new Date().getTime()-date.getTime()+ " msg");
+        
+        input.close();
+        output.flush();
+        output.close();
     }
 
     private static void print(File[] listFiles, String ident) {
