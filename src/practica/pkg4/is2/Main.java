5fe880b34364751860b40f12e37172ca2a2b5564
Salvador <Salvador@Salvador-PC>
2015-10-29 09:41:59 +0000

    Primer Commit practica 4 Histograma  (CLASE MailListReader)

--- src/practica/pkg4/is2/Main.java
+++ src/practica/pkg4/is2/Main.java
@@ -1,54 +1,15 @@
 package practica.pkg4.is2;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Date;
+import java.util.ArrayList;
+
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        String pathName = "C:\\";
-        File file = new File (pathName);
-        String[] dire = file.list();
-                
-        String from = "C:\\Users\\Salvador\\Pictures\\programando.jpg";
-        String to = "C:\\Users\\Salvador\\Pictures\\programando(1).jpg";
+    public static void main(String[] args) throws IOException{
+        String nameFile = "C:\\Users\\Salvador\\Documents\\0-Salvador\\ULPGC\\TERCERO\\Netbeans\\Practica 4 IS2\\emailsfilev1.txt";
+        ArrayList<String> mailArray = MailListReader.read(nameFile);
         
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
-        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
-        
-        byte[] buffer = new byte [1024];
-        
-        Date date = new Date();
-        while(true){
-            int read = input.read(buffer);
-            if(read < 0) break;
-            output.write(buffer);
-        }
-        System.out.println(new Date().getTime()-date.getTime()+ " msg");
-        
-        input.close();
-        output.flush();
-        output.close();
-    }
-
-    private static void print(File[] listFiles, String ident) {
-        if(listFiles == null){
-            return;
-        }
-        
-        for (File listFile : listFiles) {
-            System.out.println(ident+(listFile.isDirectory()?"+": " ")+listFile.getName());
-            if (listFile.isFile() || listFile.isHidden()) continue;
-            print(listFile.listFiles(), ident + " ");
-        }
-        
+        System.out.println(mailArray.size());
     }
     
 }
